d33bb9f6d84acc965569ca07a9f3ec0c676ea437
Makz Mann <mr.greenmousth@gmail.com>
2023-10-02 23:21:55 -0300

    remove list item with event listener

--- main.js
+++ main.js
@@ -10,9 +10,20 @@ const input = document.querySelector('input')
 function add() {
     if (input.value) {
         const li = document.createElement('li')
+        const button = document.createElement('button')
+
         li.innerText = input.value
+        button.innerText = 'x'
+
+        button.addEventListener('click', remove)
+
+        li.appendChild(button)
         ul.appendChild(li)
 
         input.value = ''
     }
+}
+
+function remove(e) {
+    e.target.parentElement.remove()
 }
