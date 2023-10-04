c199b19f6d0391e8d6ad6566d85530f2d455646e
Makz Mann <mr.greenmousth@gmail.com>
2023-10-03 23:58:14 -0300

    load the list of tasks from localStorage at the beginning

--- main.js
+++ main.js
@@ -7,18 +7,24 @@ document.querySelector('form')
 const ul = document.querySelector('ul')
 const input = document.querySelector('input')
 
+loadFromLocalStorage()
+
+function renderItem(text) {
+    const li = document.createElement('li')
+    const deleteButton = document.createElement('button')
+
+    li.innerText = text
+    deleteButton.innerText = 'x'
+
+    deleteButton.addEventListener('click', remove)
+
+    li.appendChild(deleteButton)
+    ul.appendChild(li)
+}
+
 function add() {
     if (input.value) {
-        const li = document.createElement('li')
-        const button = document.createElement('button')
-
-        li.innerText = input.value
-        button.innerText = 'x'
-
-        button.addEventListener('click', remove)
-
-        li.appendChild(button)
-        ul.appendChild(li)
+        renderItem(input.value)
 
         input.value = ''
 
@@ -38,4 +44,15 @@ function saveToLocalStorage() {
     }
 
     localStorage.setItem('todoList', JSON.stringify(items))
+}
+
+function loadFromLocalStorage() {
+    const stringItems = localStorage.getItem('todoList')
+    if (stringItems) {
+        const items = JSON.parse(stringItems)
+
+        for (item of items) {
+            renderItem(item)
+        }
+    }
 }
